bf05805feefbac116439b71bd4e29a0329b0f35f
kingorianderson <kingorianderson@gmail.com>
2022-02-15 19:30:13 +0300

    commit

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const { boolean } = require("webidl-conversions");
+
 
 const UserSchema = new mongoose.Schema(
     {
         username: { type: String, required: true, unique: true},
         email: { type: String, required: true, unique: true},
         password: { type: String, required: true},
-        isAdmin: { type: boolean, default: false,
+        isAdmin: { type: Boolean, default: false,
         },
     },
     {timestamps: true}
