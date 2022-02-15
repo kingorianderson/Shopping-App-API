d70127c322dfb21d74e30c02845b725874185528
kingorianderson <kingorianderson@gmail.com>
2022-02-15 19:59:34 +0300

    commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth"); 
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ mongoose
       console.log(err);
   });
 
+
+app.use(express.json());
+app.use("api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
