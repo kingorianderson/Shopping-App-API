76d5497be0256d1469fe1d210847ca599dc6370b
kingorianderson <kingorianderson@gmail.com>
2022-02-16 11:39:10 +0300

    commit

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -26,4 +26,15 @@ const verifyTokenAndAuthorization = (req, res, next)=> {
     });
 };
 
+const verifyTokenAndAdmin = (req, res, next)=> {
+    verifyToken(req, res, ()=>{
+        if(req.user.isAdmin){
+            next();
+        }
+        else{
+            res.status(403).json("You are not allowed to do that!");
+        }
+    });
+};
+
 module.exports = { verifyToken, verifyTokenAndAuthorization };
