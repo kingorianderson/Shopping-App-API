0cb194ba652fc42eee6d68ece2c33ca41606d3a8
kingorianderson <kingorianderson@gmail.com>
2022-02-17 16:41:05 +0300

    commit

--- routes/user.js
+++ routes/user.js
@@ -48,5 +48,15 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// GET ALL USER
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query ? await User.find().sort({_id: -1}).limits(5) : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
