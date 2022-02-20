7cc55848f440cfd283ae888a541d7643c2cf1a08
kingorianderson <kingorianderson@gmail.com>
2022-02-20 08:58:43 +0300

    commit

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,18 @@
 const router = reqiure("express").Router();
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin, } = require("./verifyToken");
+const Product = require("../models/Product");
 
+// CREATE
+route.post("/", verifyTokenAndAdmin, async (req, res) =>{
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
